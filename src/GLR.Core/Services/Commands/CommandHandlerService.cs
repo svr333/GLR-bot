c894c190c4b603cebc403beb3a67c592ad6d59b2
Senne Van Rompaey <vanrompaey.senne@protonmail.com>
2020-04-26 23:13:05 +0200

    Remove redundant using statement

--- src/GLR.Core/Services/Commands/CommandHandlerService.cs
+++ src/GLR.Core/Services/Commands/CommandHandlerService.cs
@@ -4,7 +4,6 @@ using Discord.WebSocket;
 using GLR.Core.Extensions;
 using GLR.Core.Services.DataStorage;
 using System;
-using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
