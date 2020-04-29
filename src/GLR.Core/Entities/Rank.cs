95b2784f1164bb67186714eedc6e4488eb039028
Senne Van Rompaey <vanrompaey.senne@protonmail.com>
2020-04-29 20:09:34 +0200

    Add banned rank

--- src/GLR.Core/Entities/Rank.cs
+++ src/GLR.Core/Entities/Rank.cs
@@ -4,24 +4,26 @@ namespace GLR.Core.Entities
     {
         public Rank Rank { get; set; }
         public uint ColourValue 
-        { 
+        {
             get 
             {
                 if (Rank == Rank.Developer) return 480472;
                 if (Rank == Rank.Moderator) return 2605694;
                 if (Rank == Rank.Tester) return 16729674;
                 if (Rank == Rank.Donator) return 15710778;
-                else return 16777215;
+                if (Rank == Rank.Banned) return 16777215;
+                else return 000000;
             }
         }
     }
 
     public enum Rank
     {
-        Member = 0,
-        Donator = 1,
-        Tester = 2,
-        Moderator = 3,
+        Banned = 0,
+        Member = 1,
+        Donator = 2,
+        Tester = 3,
+        Moderator = 4,
         Developer = 5
     }
 }
