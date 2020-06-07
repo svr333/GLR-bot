a5a933d57ef932f2b9c32d9622743d747dc1b6f2
Senne Van Rompaey <vanrompaey.senne@protonmail.com>
2020-06-07 19:27:40 +0200

    Implement buildawall command

--- src/GLR.Core/Commands/Modules/ProfileModule.cs
+++ src/GLR.Core/Commands/Modules/ProfileModule.cs
@@ -94,7 +94,7 @@ namespace GLR.Core.Commands.Modules
 
             if (leaderboardType == "chips") displayTexts = (await _client.GetTopChipsPlayers()).ToArray();
             else if (leaderboardType == "levels") displayTexts = (await _client.GetTopLevelPlayers()).ToArray();
-            else throw new Exception("Wrong leaderboard type. Either choose `levels´ or `chips´.");
+            else throw new Exception("Wrong leaderboard type. Either choose `levels´ or `chips`.");
             
             for (int i = 0; i < displayTexts.Count(); i++)
             {
@@ -113,6 +113,8 @@ namespace GLR.Core.Commands.Modules
         [Command("compare")][Alias("c")]
         public async Task CompareStats(string baseUser, string secondUser)
         {
+            if (baseUser.ToLower() == secondUser.ToLower()) throw new Exception("Please enter two different users to compare.");
+
             var baseUserId = await _client.GetIdAsync(baseUser);
             var baseUserStats = await _client.GetStatisticsAsync(baseUserId);
 
@@ -125,9 +127,9 @@ namespace GLR.Core.Commands.Modules
             {
                 Title = $"Comparison between {baseUserStats.Username} & {secondUserStats.Username}",
                 Description = $"{baseUserStats.Username} has **{expDifference}x** the experience of {secondUserStats.Username}\n" +
-                              $"Difference of **{FormatExperience(Math.Abs((decimal)baseUserStats.ExperiencePoints - secondUserStats.ExperiencePoints))}** experience.\n\n" + 
-                              $"{baseUserStats.Username} has **{FormatExperience(baseUserStats.ExperiencePoints)}** experience and is level **{baseUserStats.Level}**.\n" +
-                              $"{secondUserStats.Username} has **{FormatExperience(secondUserStats.ExperiencePoints)}** experience and is level **{secondUserStats.Level}**.",
+                              $"Difference of **{FormatNumbers(Math.Abs((decimal)baseUserStats.ExperiencePoints - secondUserStats.ExperiencePoints))}** experience.\n\n" + 
+                              $"{baseUserStats.Username} has **{FormatNumbers(baseUserStats.ExperiencePoints)}** experience and is level **{baseUserStats.Level}**.\n" +
+                              $"{secondUserStats.Username} has **{FormatNumbers(secondUserStats.ExperiencePoints)}** experience and is level **{secondUserStats.Level}**.",
                 Color = expDifference > 1 ? Color.DarkGreen : Color.DarkOrange
             }
             .Build());
@@ -136,8 +138,71 @@ namespace GLR.Core.Commands.Modules
         [Command("compare")][Alias("c")]
         public async Task CompareStats(string userToCompare)
             => await CompareStats(Context.User.Username, userToCompare);
-    
-        private string FormatExperience(decimal experiencePoints)
+
+        [Command("buildawall")]
+        public async Task GetNecessaryExpForLevel(string baseUser, string userToCompare, [Remainder]string fact = "")
+        {
+            if (baseUser.ToLower() == userToCompare.ToLower()) throw new Exception("Please enter two different users to compare.");
+
+            var baseUserId = await _client.GetIdAsync(baseUser);
+            var baseUserStats = await _client.GetStatisticsAsync(baseUserId);
+
+            var secondUserId = await _client.GetIdAsync(userToCompare);
+            var secondUserStats = await _client.GetStatisticsAsync(secondUserId);
+
+            var expToGain = secondUserStats.ExperiencePoints - baseUserStats.ExperiencePoints;
+            if (expToGain < 0) throw new Exception($"{baseUserStats.Username} is already ahead of {secondUserStats.Username}");
+            if (expToGain == 0) throw new Exception($"{baseUserStats.Username} & {secondUserStats.Username} have equally as much exp.");
+            
+            await ReplyAsync("", false, new EmbedBuilder()
+            {
+                Title = $"Exp needed to beat {secondUserStats.Username}",
+                Description = $"{baseUserStats.Username} has **{FormatNumbers(baseUserStats.ExperiencePoints)}** experience and is level **{baseUserStats.Level}**.\n" +
+                              $"{secondUserStats.Username} has **{FormatNumbers(secondUserStats.ExperiencePoints)}** experience and is level **{secondUserStats.Level}**.\n\n" +
+                              $"{GetVisualizationForProgressNecessary(expToGain, fact)}",
+                Color = Color.DarkMagenta
+            }
+            .Build());
+        }
+
+        private string GetVisualizationForProgressNecessary(decimal expDifference, string fact = "")
+        {
+            var r = new Random();
+
+            uint wallToLevel5Price = 248000;
+            uint expPerWallToLevel5 = 641;
+
+            uint sbToLevel9Price = 121000000;
+            uint expPerSbToLevel9 = 313701;
+
+            uint expPerFirebitAttack = 38990;
+
+            uint expPerWarpGateLevel6Destroyed = 3541;
+
+            if (fact == "") 
+            {
+                var random = r.Next(0, 3);
+
+                if (random == 0) fact = "firebit";
+                else if (random == 1) fact = "walls";
+                else if (random == 2) fact = "sb";
+                else if (random == 3) fact = "warp gates";
+            }
+
+            switch (fact)
+            {
+                case "firebit": return $"User needs to fully defeat firebit **{FormatNumbers(Math.Round(expDifference / expPerFirebitAttack, 0))}** more times to beat them.";
+                case "walls": return $"User needs to upgrade **{FormatNumbers(Math.Round(expDifference / expPerWallToLevel5, 0))}** more walls from lvl 4 to 5 to beat them.\n" +
+                                     $"This would cost **{FormatNumbers(Math.Round((expDifference / expPerWallToLevel5) * wallToLevel5Price, 0))}** gold";
+                case "sb": return $"User needs to upgrade **{FormatNumbers(Math.Round(expDifference / expPerSbToLevel9, 0))}** more starbases from level 8 to 9 to beat them.\n" + 
+                                  $"This would cost **{FormatNumbers(Math.Round((expDifference / expPerSbToLevel9) * sbToLevel9Price, 0))}** gold.";
+                case "warp gates": return $"User would need to destroy **{FormatNumbers(Math.Round(expDifference / expPerWarpGateLevel6Destroyed))}** more warp gates lvl 6 to beat them.";
+                
+                default: return $"Please enter a valid value (`firebit`, `walls`, `sb` or `warp gates`).";
+            }
+        }
+
+        private string FormatNumbers(decimal experiencePoints)
         {
             // 10mil< 
             if (experiencePoints > 10000000) return $"{Math.Round(experiencePoints / 1000000, 1)}M";
